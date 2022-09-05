9ba70f60615803f161f7703d0cf9b04b82d265e4
MyUsername <robertomazza99@gmail.com>
2022-09-05 15:59:07 +0200

    add 'echo' on each test

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
     include_once __DIR__.'./NotRegisteredCustomer.php';
     include_once __DIR__.'./Product.php';
 
+    echo 'TEST CLASSE PRODOTTO';
     $product= new Product('ciccio','pasticcio','cibo',150);
     var_dump($product);
 
+    echo 'TEST CLASSE CLIENTE';
     $customer = new RegisteredCustomer(6555,'15/05/25','Via emilia','ciccio@pasticcio.com','nonteladico','pasta','m','prosciutto');
     var_dump($customer);
 
+    echo 'TEST INSERIMENTO PRODOTTI';
     $customer->AddProducts(new Product('dasd','dsda','dsda',20));
     var_dump($customer);
 
